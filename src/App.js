edd8b846252a4285d47d8c9ddf4ff45ed87f55a2
emb <esneidermo@gmail.com>
2022-05-06 04:45:43 +0200

    dandole_formato

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
+import logo from "./imagenes/logo.png";
+
 
 function App() {
   return (
     <div className="aplicacion-tarea">
-     
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={logo}
+          className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-listas-principal'>
+        <h1>Mis Tareas</h1>
+      </div>
     </div>
   );
 }
